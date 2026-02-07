60425f3374ca31f728ea5ac0b262df6b1d608423
felipenehuengutierrez <felipe.nehueng@gmail.com>
2026-02-07 07:32:27 +0000

    gdd

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let dominio = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        dominio.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+
+  let resultado = Math.floor(Math.random() * dominio.length);
+
+  document.write(dominio[resultado]);
 };
